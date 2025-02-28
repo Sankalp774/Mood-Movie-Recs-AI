7e0203cbdf3464da5353bfebc892b184069641f1
Sankalp Sahu <sankalp774sahu@gmail.com>
2025-02-28 08:59:51 +0530

    Create Movie.kt
    **Movie.kt** defines a data class representing a movie with properties like ID, title, year, rating, genre, description, and a local image resource ID. This serves as the model for handling movie data in the app.

---
+++ Movie.kt
@@ -0,0 +1,11 @@
+package com.example.imdbclone
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val year: String,
+    val rating: Float,
+    val genre: String,
+    val description: String,
+    val imageResId: Int // We'll use local images for now
+)
