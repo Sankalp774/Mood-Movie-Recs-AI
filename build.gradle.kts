8acc131de8b91f2d19fb8dc183d43c04fbbba33d
Sankalp Sahu <sankalp774sahu@gmail.com>
2025-05-02 23:28:50 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,5 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
     
 }
