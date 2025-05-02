9d2e03c0c325c95e43fdd571f6298e0cd598da94
Sankalp Sahu <sankalp774sahu@gmail.com>
2025-05-02 23:31:28 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation(platform("androidx.compose:comp
