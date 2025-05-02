f2d89798059a0ff092e01bfd9bcd616b0ff3e533
Sankalp Sahu <sankalp774sahu@gmail.com>
2025-05-02 23:29:08 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.c3:material3")
     // Retrofit for API calls
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
